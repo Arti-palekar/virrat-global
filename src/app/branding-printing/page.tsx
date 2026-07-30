05766f4bb5f1439039acaadb11cbeace39aaee1f
Developer <dev@virratglobal.com>
2026-07-30 18:03:58 +0530

    Remove old BPHeroParallax portfolio section and place ImageGallery (What We Create Services Showcase) immediately before BPExpertise on Branding & Printing page

--- src/app/branding-printing/page.tsx
+++ src/app/branding-printing/page.tsx
@@ -7,7 +7,6 @@ import BPExpertise from "./components/BPExpertise";
 import BPServicesShowcase from "./components/BPServicesShowcase";
 import { ImageGallery } from "./components/ImageGallery";
 import BPMarqueeStrip from "./components/BPMarqueeStrip";
-import BPHeroParallax from "./components/BPHeroParallax";
 import BPContactCTA from "./components/BPContactCTA";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FAQ1 from "@/components/FAQ1";
@@ -22,23 +21,24 @@ export default function BrandingPrintingPage() {
         {/* 1. Hero Section */}
         <BPProcess />
 
-        {/* 2. Branding & Printing Sections */}
+        {/* 2. What We Create Section */}
         <ImageGallery />
+
+        {/* 3. Branding & Printing Sections */}
         <BPExpertise />
         <BPServicesShowcase />
         <BPMarqueeStrip />
-        <BPHeroParallax />
         <BPContactCTA />
 
-        {/* 3. Testimonials Section (same as Home) */}
+        {/* 4. Testimonials Section (same as Home) */}
         <TestimonialsSection />
 
-        {/* 4. FAQ Section (same as Home) */}
+        {/* 5. FAQ Section (same as Home) */}
         <FAQ1 />
 
       </main>
 
-      {/* 5. Global Footer */}
+      {/* 6. Global Footer */}
       <CinematicFooter />
     </>
   );
