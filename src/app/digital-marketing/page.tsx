e63d0294a70774e3f33500bfdce0f2266a56f461
Developer <dev@virratglobal.com>
2026-08-05 14:48:28 +0530

    feat: add clip-path social links component after service section on digital-marketing page

--- src/app/digital-marketing/page.tsx
+++ src/app/digital-marketing/page.tsx
@@ -11,6 +11,7 @@ import FAQ1 from "@/components/FAQ1";
 import DMHero from "./components/DMHero";
 import ColorfulBentoGrid from "@/components/ui/colorful-bento-grid";
 import DMTopPicks from "./components/DMTopPicks";
+import { ClipPathLinks } from "@/components/ui/clip-path-links";
 import DMTools from "./components/DMTools";
 import DMProcess from "./components/DMProcess";
 import DMConsiderations from "./components/DMConsiderations";
@@ -116,6 +117,9 @@ export default function DigitalMarketingPage() {
 
         {/* Top Picks Showcase Section */}
         <DMTopPicks />
+
+        {/* Clip-Path Ecosystem Links */}
+        <ClipPathLinks />
 
         {/* Tools We Use Section */}
         <DMTools />
